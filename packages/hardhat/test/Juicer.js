097f40d4729dcd88d4b5b291b45212c0fddb57e3
Nerve Trip <nerve@nervetrip.com>
2021-04-02 00:53:03 -0700

    initial juicer deploy tests

--- packages/hardhat/test/Juicer.js
+++ packages/hardhat/test/Juicer.js
@@ -1,41 +1,109 @@
-// const { use, expect } = require("chai");
-const { expect } = require("chai");
-
+const { ethers } = require("hardhat");
+const { use, expect } = require("chai");
 const weth = require("../constants/weth");
 const deploy = require("../test-helpers/deploy");
+const { solidity, createFixtureLoader } = require("ethereum-waffle");
 
-// const { solidity } = require("ethereum-waffle");
-// use(solidity);
+use(solidity);
+
+const MONTH = 60*60*24*30; // 30 days
 
 describe("Juicer", () => {
+  let token;
+  let prices;
+  let projects;
+  let budgetStore;
+  let ticketStore;
   let juicer;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
 
   beforeEach(async () => {
-    const token = await deploy("Token");
-    const prices = await deploy("Prices");
-    const projects = await deploy("Projects");
-    const budgetStore = await deploy("BudgetStore", [prices.address]);
-    const ticketStore = await deploy("TicketStore");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+    token = await deploy("Token");
+    prices = await deploy("Prices");
+    projects = await deploy("Projects");
+    budgetStore = await deploy("BudgetStore", [prices.address]);
+    ticketStore = await deploy("TicketStore");
     juicer = await deploy("Juicer", [
       projects.address,
       budgetStore.address,
       ticketStore.address,
       weth(process.env.HARDHAT_NETWORK) || token.address,
     ]);
-
+    // console.log(`Owner: ${owner.address}`);
+    // console.log(`Addr1: ${addr1.address}`);
+    // console.log(`Contract address: ${juicer.address}`);
+    // const balance = await addr1.getBalance()
+    // console.log(`Addr1 balance: ${balance}`);
+    
     await juicer.deployed();
   });
 
-  it("Should initialize with no overflow", async () => {
-    expect(await juicer.getTotalOverflow()).to.equal("0");
+  describe("Contract Deployment", function () {
+    it("Should set the right owner", async function () {
+      // expect(await juicer.owner()).to.equal(owner.address);
+    });
+
+    it("Should initialize with no overflow", async () => {
+      expect(await juicer.getTotalOverflow()).to.equal("0");
+    });
+  });    
+
+  describe("deploy()", () => {
+    it("Should deploy a project", async () => {
+      const projectOwner = addr1;
+      const name = "Test Project";
+      const handle = "TEST";
+      const link = "http://test/";
+      const logoURI = `${link}logo.png`;
+      const target = 1000; // $1000
+      const currency = 1;  // USD
+      const duration = MONTH; 
+      const discountRate = 1000; // 100%
+      const bondingCurve = 1000; // 100%
+      const reserved = 50;  // 5%
+
+      // FIXME: Throws exception:
+      // ProviderError: VM Exception while processing transaction: revert Administrated: UNAUTHORIZED
+      let tx = await juicer.connect(projectOwner).deploy(
+        projectOwner.address,
+        name,
+        handle, 
+        logoURI,
+        target,
+        currency,
+        duration,
+        link,
+        discountRate,
+        bondingCurve,
+        reserved
+      );
+
+      // Verify event
+      let receipt = await tx.wait();
+      const event = receipt.events.filter((x) => {return x.event == "Deploy"});
+      console.log(event);
+
+      // Verify  project
+      const identifiers = await projects.getIdentifier(event.projectId);
+      expect(identifiers.name, name);
+      expect(identifiers.handle, handle);
+      expect(identifiers.logoURI, logoURI);
+      expect(identifiers.link, link);
+
+      // Verify budgetStore
+      const budget = await budgetStore.getBudget(event.budgetId /* Prop doesn't exist */);
+      expect(budget.projectId, event.projectId);
+      expect(budget.target, target);
+      expect(identifiers.currency, currency);
+      expect(identifiers.duration, duration);
+      expect(identifiers.reserved, reserved);
+      expect(identifiers.bondingCurve, bondingCurve);
+      expect(identifiers.discountRate, discountRate);
+
+    });
   });
-
-  // describe("setPurpose()", () => {
-  //   it("Should be able to set a new purpose", async () => {
-  //     const newPurpose = "Test Purpose";
-
-  //     await myContract.setPurpose(newPurpose);
-  //     expect(await myContract.purpose()).to.equal(newPurpose);
-  //   });
-  // });
 });
