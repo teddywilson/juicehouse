e07996f1ec0ee4e4c0657566e0c8144053c28823
Nerve Trip <nerve@nervetrip.com>
2021-04-04 21:17:53 -0700

    Set setup working but not optimized

--- packages/hardhat/test/Juicer.js
+++ packages/hardhat/test/Juicer.js
@@ -7,8 +7,10 @@ const { solidity, createFixtureLoader } = require("ethereum-waffle");
 use(solidity);
 
 const MONTH = 60*60*24*30; // 30 days
+const blockGasLimit = 9000000;
 
 describe("Juicer", () => {
+  let admin;
   let token;
   let prices;
   let projects;
@@ -19,7 +21,8 @@ describe("Juicer", () => {
   let addr1;
   let addr2;
   let addrs;
-
+  
+  // TODO: Running tests is slow because lots of setup must run before every test
   beforeEach(async () => {
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
     token = await deploy("Token");
@@ -33,6 +36,11 @@ describe("Juicer", () => {
       ticketStore.address,
       weth(process.env.HARDHAT_NETWORK) || token.address,
     ]);
+    admin = await deploy("Admin", [
+      juicer.address,
+      "0x766621e1e1274496ab3d65badc5866024f1ab7b8",
+    ]);
+
     // console.log(`Owner: ${owner.address}`);
     // console.log(`Addr1: ${addr1.address}`);
     // console.log(`Contract address: ${juicer.address}`);
@@ -40,6 +48,74 @@ describe("Juicer", () => {
     // console.log(`Addr1 balance: ${balance}`);
     
     await juicer.deployed();
+
+    // TODO: Figure out what is necessary and what isn't
+    try {
+      const ProjectsFactory = await ethers.getContractFactory("Projects");
+      const TicketStoreFactory = await ethers.getContractFactory("TicketStore");
+      const BudgetStoreFactory = await ethers.getContractFactory("BudgetStore");
+      const PricesFactory = await ethers.getContractFactory("Prices");
+      const AdminFactory = await ethers.getContractFactory("Admin");
+      // const StakerFactory = await ethers.getContractFactory("TimelockStaker");
+      const JuicerFactory = await ethers.getContractFactory("Juicer");
+  
+      const attachedProjects = await ProjectsFactory.attach(projects.address);
+      const attachedTicketStore = await TicketStoreFactory.attach(
+        ticketStore.address
+      );
+      const attachedBudgetStore = await BudgetStoreFactory.attach(
+        budgetStore.address
+      );
+      const attachedPrices = await PricesFactory.attach(prices.address);
+      const attachedAdmin = await AdminFactory.attach(admin.address);
+      // const attachedStaker = await StakerFactory.attach(staker.address);
+      const attachedJuicer = await JuicerFactory.attach(juicer.address);
+  
+      await attachedProjects.setOwnership(admin.address, {
+        gasLimit: blockGasLimit,
+      });
+      await attachedTicketStore.setOwnership(admin.address, {
+        gasLimit: blockGasLimit,
+      });
+      await attachedBudgetStore.setOwnership(admin.address, {
+        gasLimit: blockGasLimit,
+      });
+      await attachedPrices.transferOwnership(admin.address, {
+        gasLimit: blockGasLimit,
+      });
+      await attachedAdmin.grantAdmin(projects.address, juicer.address, {
+        gasLimit: blockGasLimit,
+      });
+      await attachedAdmin.grantAdmin(budgetStore.address, juicer.address, {
+        gasLimit: blockGasLimit,
+      });
+      await attachedAdmin.grantAdmin(ticketStore.address, juicer.address, {
+        gasLimit: blockGasLimit,
+      });
+      await attachedJuicer.setAdmin(admin.address, {
+        gasLimit: blockGasLimit,
+      });
+      // Create the admin's budget.
+      await attachedJuicer.deploy(
+        admin.address,
+        "Juice",
+        "juice",
+        "https://some.jpg",
+        "0x3635C9ADC5DEA00000",
+        1,
+        2592000,
+        "https://asdf.com",
+        970,
+        382,
+        50,
+        {
+          gasLimit: blockGasLimit,
+        }
+      );
+    } catch (e) {
+      console.log("Failed to establish admin contract ownership: ", e);
+    }
+
   });
 
   describe("Contract Deployment", function () {
@@ -58,51 +134,55 @@ describe("Juicer", () => {
       const name = "Test Project";
       const handle = "TEST";
       const link = "http://test/";
-      const logoURI = `${link}logo.png`;
+      const logoUri = `${link}logo.png`;
       const target = 1000; // $1000
       const currency = 1;  // USD
       const duration = MONTH; 
       const discountRate = 1000; // 100%
-      const bondingCurve = 1000; // 100%
+      const bondingCurveRate = 1000; // 100%
       const reserved = 50;  // 5%
 
-      // FIXME: Throws exception:
-      // ProviderError: VM Exception while processing transaction: revert Administrated: UNAUTHORIZED
       let tx = await juicer.connect(projectOwner).deploy(
         projectOwner.address,
         name,
         handle, 
-        logoURI,
+        logoUri,
         target,
         currency,
         duration,
         link,
         discountRate,
-        bondingCurve,
+        bondingCurveRate,
         reserved
       );
+      let receipt = await tx.wait();
 
       // Verify event
-      let receipt = await tx.wait();
-      const event = receipt.events.filter((x) => {return x.event == "Deploy"});
-      console.log(event);
-
+      const events = receipt.events.filter(x => x.event === "Deploy");
+      expect(events).to.have.lengthOf(1);
+      const event = events[0];
+      expect(event).to.have.property('event').that.equals('Deploy');
+      
       // Verify  project
-      const identifiers = await projects.getIdentifier(event.projectId);
-      expect(identifiers.name, name);
-      expect(identifiers.handle, handle);
-      expect(identifiers.logoURI, logoURI);
-      expect(identifiers.link, link);
+      // TODO: Is there a better way to get the projectId?
+      const projectId = event.args.projectId;
+      const identifiers = await projects.getIdentifier(projectId);
+      expect(identifiers.name).to.equal(name);
+      expect(identifiers.handle).to.equal(handle);
+      expect(identifiers.logoUri).to.equal(logoUri);
+      expect(identifiers.link).to.equal(link);
 
       // Verify budgetStore
-      const budget = await budgetStore.getBudget(event.budgetId /* Prop doesn't exist */);
-      expect(budget.projectId, event.projectId);
-      expect(budget.target, target);
-      expect(identifiers.currency, currency);
-      expect(identifiers.duration, duration);
-      expect(identifiers.reserved, reserved);
-      expect(identifiers.bondingCurve, bondingCurve);
-      expect(identifiers.discountRate, discountRate);
+      // TODO: Is there a better way to get the budgetId?
+      const budgetId = event.args.budget.id;
+      const budget = await budgetStore.getBudget(budgetId);
+      expect(budget.projectId).to.equal(projectId);
+      expect(budget.target).to.equal(target);
+      expect(budget.currency).to.equal(currency);
+      expect(budget.duration).to.equal(duration);
+      expect(budget.reserved).to.equal(reserved);
+      expect(budget.bondingCurveRate).to.equal(bondingCurveRate);
+      expect(budget.discountRate).to.equal(discountRate);
 
     });
   });
