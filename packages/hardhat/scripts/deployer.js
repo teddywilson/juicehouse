1002b146656ff44c3d824e4c73387aa0616155a9
Nerve Trip <nerve@nervetrip.com>
2021-04-07 23:11:47 -0700

    Reuse deploy script in tests

---
+++ packages/hardhat/scripts/deployer.js
@@ -0,0 +1,224 @@
+/* eslint no-use-before-define: "warn" */
+const fs = require("fs");
+const chalk = require("chalk");
+const { l2ethers, ethers } = require("hardhat");
+const { utils } = require("ethers");
+const R = require("ramda");
+const ethUsdPriceFeed = require("../constants/eth_usd_price_feed");
+
+module.exports = async (wethAddr, ethUsdAddr) => {
+  const token = !wethAddr && (await deploy("Token"));
+  const prices = await deploy("Prices");
+  const projects = await deploy("Projects");
+  const budgetStore = await deploy("BudgetStore", [prices.address]);
+  const ticketStore = await deploy("TicketStore");
+
+  const juicer = await deploy("Juicer", [
+    projects.address,
+    budgetStore.address,
+    ticketStore.address,
+    wethAddr || token.address,
+  ]);
+
+  // const staker = await deploy("TimelockStaker");
+  // const budgetBallot = await deploy("BudgetBallot", [
+  //   juicer.address,
+  //   staker.address,
+  // ]);
+
+  const admin = await deploy("Admin", [
+    juicer.address,
+    "0x766621e1e1274496ab3d65badc5866024f1ab7b8",
+  ]);
+
+  const blockGasLimit = 9000000;
+
+  try {
+    const ProjectsFactory = await ethers.getContractFactory("Projects");
+    const TicketStoreFactory = await ethers.getContractFactory("TicketStore");
+    const BudgetStoreFactory = await ethers.getContractFactory("BudgetStore");
+    const PricesFactory = await ethers.getContractFactory("Prices");
+    const AdminFactory = await ethers.getContractFactory("Admin");
+    // const StakerFactory = await ethers.getContractFactory("TimelockStaker");
+    const JuicerFactory = await ethers.getContractFactory("Juicer");
+
+    const attachedProjects = await ProjectsFactory.attach(projects.address);
+    const attachedTicketStore = await TicketStoreFactory.attach(
+      ticketStore.address
+    );
+    const attachedBudgetStore = await BudgetStoreFactory.attach(
+      budgetStore.address
+    );
+    const attachedPrices = await PricesFactory.attach(prices.address);
+    const attachedAdmin = await AdminFactory.attach(admin.address);
+    // const attachedStaker = await StakerFactory.attach(staker.address);
+    const attachedJuicer = await JuicerFactory.attach(juicer.address);
+
+    console.log("⚡️ Setting the projects owner");
+    await attachedProjects.setOwnership(admin.address, {
+      gasLimit: blockGasLimit,
+    });
+    console.log("⚡️ Setting the ticket store owner");
+    await attachedTicketStore.setOwnership(admin.address, {
+      gasLimit: blockGasLimit,
+    });
+    console.log("⚡️ Setting the budget store owner");
+    await attachedBudgetStore.setOwnership(admin.address, {
+      gasLimit: blockGasLimit,
+    });
+    console.log("⚡️ Setting the prices owner");
+    await attachedPrices.transferOwnership(admin.address, {
+      gasLimit: blockGasLimit,
+    });
+    // console.log("⚡️ Setting the staker owner");
+    // await attachedStaker.setOwnership(admin.address, {
+    //   gasLimit: blockGasLimit,
+    // });
+    console.log("⚡️ Granting the juicer admin privileges over the projects");
+    await attachedAdmin.grantAdmin(projects.address, juicer.address, {
+      gasLimit: blockGasLimit,
+    });
+    console.log(
+      "⚡️ Granting the juicer admin privileges over the budget store"
+    );
+    await attachedAdmin.grantAdmin(budgetStore.address, juicer.address, {
+      gasLimit: blockGasLimit,
+    });
+    console.log(
+      "⚡️ Granting the juicer admin privileges over the ticket store"
+    );
+    await attachedAdmin.grantAdmin(ticketStore.address, juicer.address, {
+      gasLimit: blockGasLimit,
+    });
+    if (ethUsdAddr) {
+      console.log("⚡️ Adding ETH/USD price feed to the budget store");
+      await attachedAdmin.addPriceFeed(
+        budgetStore.address,
+        ethUsdAddr,
+        1,
+        {
+          gasLimit: blockGasLimit,
+        }
+      );
+    }
+
+    console.log("⚡️ Setting the admin of the juicer");
+    await attachedJuicer.setAdmin(admin.address, {
+      gasLimit: blockGasLimit,
+    });
+
+    // TODO set the owner of the admin contract.
+    // await attachedJuicer.transferOwnership(admin.address, {
+    //   gasLimit: blockGasLimit
+    // });
+
+    // console.log("⚡️ Setting the budget ballot as a controller of the staker");
+    // // Make this Ballot a timelock controller of the staker contract.
+    // await attachedAdmin.setControllerStatus(
+    //   staker.address,
+    //   budgetBallot.address,
+    //   true,
+    //   {
+    //     gasLimit: blockGasLimit,
+    //   }
+    // );
+
+    console.log("⚡️ Configuring the admins budget");
+    // Create the admin's budget.
+    await attachedJuicer.deploy(
+      admin.address,
+      "Juice",
+      "juice",
+      "https://some.jpg",
+      "0x3635C9ADC5DEA00000",
+      1,
+      2592000,
+      "https://asdf.com",
+      970,
+      382,
+      50,
+      {
+        gasLimit: blockGasLimit,
+      }
+    );
+  } catch (e) {
+    console.log("Failed to establish admin contract ownership: ", e);
+  }
+
+  console.log(
+    " 💾  Artifacts (address, abi, and args) saved to: ",
+    chalk.blue("packages/hardhat/artifacts/"),
+    "\n\n"
+  );
+
+  return {
+    admin,
+    token,
+    prices,
+    projects,
+    budgetStore,
+    ticketStore,
+    juicer
+  };
+};
+
+const deploy = async (contractName, _args) => {
+  console.log(` 🛰  Deploying: ${contractName}`);
+
+  const contractArgs = _args || [];
+  const contractArtifacts = await ethers.getContractFactory(contractName);
+  const deployed = await contractArtifacts.deploy(...contractArgs);
+  await deployed.deployTransaction.wait();
+
+  const encoded = abiEncodeArgs(deployed, contractArgs);
+  fs.writeFileSync(`artifacts/${contractName}.address`, deployed.address);
+
+  console.log(
+    " 📄",
+    chalk.cyan(contractName),
+    "deployed to:",
+    chalk.magenta(deployed.address)
+  );
+
+  if (!encoded || encoded.length <= 2) return deployed;
+  fs.writeFileSync(`artifacts/${contractName}.args`, encoded.slice(2));
+
+  return deployed;
+};
+  
+// ------ utils -------
+
+// abi encodes contract arguments
+// useful when you want to manually verify the contracts
+// for example, on Etherscan
+const abiEncodeArgs = (deployed, contractArgs) => {
+  // not writing abi encoded args if this does not pass
+  if (
+    !contractArgs ||
+    !deployed ||
+    !R.hasPath(["interface", "deploy"], deployed)
+  ) {
+    return "";
+  }
+  const encoded = utils.defaultAbiCoder.encode(
+    deployed.interface.deploy.inputs,
+    contractArgs
+  );
+  return encoded;
+};
+
+// checks if it is a Solidity file
+const isSolidity = (fileName) =>
+  fileName.indexOf(".sol") >= 0 && fileName.indexOf(".swp") < 0;
+
+const readArgsFile = (contractName) => {
+  let args = [];
+  try {
+    const argsFile = `./contracts/${contractName}.args`;
+    if (!fs.existsSync(argsFile)) return args;
+    args = JSON.parse(fs.readFileSync(argsFile));
+  } catch (e) {
+    console.log(e);
+  }
+  return args;
+};
--- packages/hardhat/test/Juicer.js
+++ packages/hardhat/test/Juicer.js
@@ -1,132 +1,38 @@
 const { ethers } = require("hardhat");
 const { use, expect } = require("chai");
-const weth = require("../constants/weth");
-const deploy = require("../test-helpers/deploy");
 const { solidity, createFixtureLoader } = require("ethereum-waffle");
+const deployer = require("../scripts/deployer");
 
 use(solidity);
 
-const MONTH = 60*60*24*30; // 30 days
-const blockGasLimit = 9000000;
+const MONTH = 60 * 60 * 24 * 30; // 30 days
+
+let owner;
+let addr1;
+let addr2;
+let addrs;
+let contracts;
+
+// Deploy contracts once before running tests
+// WARNING: For performance reasons, contracts are deployed only once before running
+// tests. However, this does mean that state may be different for each test. Shouild
+// revisit this to determine which contracts can be deployed once and which should be
+// deployed before each test.
+before(async () => {
+  [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+  contracts = await deployer();
+});
 
 describe("Juicer", () => {
-  let admin;
-  let token;
-  let prices;
-  let projects;
-  let budgetStore;
-  let ticketStore;
-  let juicer;
-  let owner;
-  let addr1;
-  let addr2;
-  let addrs;
-  
-  // TODO: Running tests is slow because lots of setup must run before every test
-  beforeEach(async () => {
-    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
-    token = await deploy("Token");
-    prices = await deploy("Prices");
-    projects = await deploy("Projects");
-    budgetStore = await deploy("BudgetStore", [prices.address]);
-    ticketStore = await deploy("TicketStore");
-    juicer = await deploy("Juicer", [
-      projects.address,
-      budgetStore.address,
-      ticketStore.address,
-      weth(process.env.HARDHAT_NETWORK) || token.address,
-    ]);
-    admin = await deploy("Admin", [
-      juicer.address,
-      "0x766621e1e1274496ab3d65badc5866024f1ab7b8",
-    ]);
-
-    // console.log(`Owner: ${owner.address}`);
-    // console.log(`Addr1: ${addr1.address}`);
-    // console.log(`Contract address: ${juicer.address}`);
-    // const balance = await addr1.getBalance()
-    // console.log(`Addr1 balance: ${balance}`);
-    
-    await juicer.deployed();
-
-    // TODO: Figure out what is necessary and what isn't
-    try {
-      const ProjectsFactory = await ethers.getContractFactory("Projects");
-      const TicketStoreFactory = await ethers.getContractFactory("TicketStore");
-      const BudgetStoreFactory = await ethers.getContractFactory("BudgetStore");
-      const PricesFactory = await ethers.getContractFactory("Prices");
-      const AdminFactory = await ethers.getContractFactory("Admin");
-      // const StakerFactory = await ethers.getContractFactory("TimelockStaker");
-      const JuicerFactory = await ethers.getContractFactory("Juicer");
-  
-      const attachedProjects = await ProjectsFactory.attach(projects.address);
-      const attachedTicketStore = await TicketStoreFactory.attach(
-        ticketStore.address
-      );
-      const attachedBudgetStore = await BudgetStoreFactory.attach(
-        budgetStore.address
-      );
-      const attachedPrices = await PricesFactory.attach(prices.address);
-      const attachedAdmin = await AdminFactory.attach(admin.address);
-      // const attachedStaker = await StakerFactory.attach(staker.address);
-      const attachedJuicer = await JuicerFactory.attach(juicer.address);
-  
-      await attachedProjects.setOwnership(admin.address, {
-        gasLimit: blockGasLimit,
-      });
-      await attachedTicketStore.setOwnership(admin.address, {
-        gasLimit: blockGasLimit,
-      });
-      await attachedBudgetStore.setOwnership(admin.address, {
-        gasLimit: blockGasLimit,
-      });
-      await attachedPrices.transferOwnership(admin.address, {
-        gasLimit: blockGasLimit,
-      });
-      await attachedAdmin.grantAdmin(projects.address, juicer.address, {
-        gasLimit: blockGasLimit,
-      });
-      await attachedAdmin.grantAdmin(budgetStore.address, juicer.address, {
-        gasLimit: blockGasLimit,
-      });
-      await attachedAdmin.grantAdmin(ticketStore.address, juicer.address, {
-        gasLimit: blockGasLimit,
-      });
-      await attachedJuicer.setAdmin(admin.address, {
-        gasLimit: blockGasLimit,
-      });
-      // Create the admin's budget.
-      await attachedJuicer.deploy(
-        admin.address,
-        "Juice",
-        "juice",
-        "https://some.jpg",
-        "0x3635C9ADC5DEA00000",
-        1,
-        2592000,
-        "https://asdf.com",
-        970,
-        382,
-        50,
-        {
-          gasLimit: blockGasLimit,
-        }
-      );
-    } catch (e) {
-      console.log("Failed to establish admin contract ownership: ", e);
-    }
-
-  });
-
   describe("Contract Deployment", function () {
     it("Should set the right owner", async function () {
-      // expect(await juicer.owner()).to.equal(owner.address);
+      // expect(await contracts.juicer.owner()).to.equal(owner.address);
     });
 
     it("Should initialize with no overflow", async () => {
-      expect(await juicer.getTotalOverflow()).to.equal("0");
+      expect(await contracts.juicer.getTotalOverflow()).to.equal("0");
     });
-  });    
+  });
 
   describe("deploy()", () => {
     it("Should deploy a project", async () => {
@@ -136,37 +42,39 @@ describe("Juicer", () => {
       const link = "http://test/";
       const logoUri = `${link}logo.png`;
       const target = 1000; // $1000
-      const currency = 1;  // USD
-      const duration = MONTH; 
+      const currency = 1; // USD
+      const duration = MONTH;
       const discountRate = 1000; // 100%
       const bondingCurveRate = 1000; // 100%
-      const reserved = 50;  // 5%
+      const reserved = 50; // 5%
 
-      let tx = await juicer.connect(projectOwner).deploy(
-        projectOwner.address,
-        name,
-        handle, 
-        logoUri,
-        target,
-        currency,
-        duration,
-        link,
-        discountRate,
-        bondingCurveRate,
-        reserved
-      );
-      let receipt = await tx.wait();
+      const tx = await contracts.juicer
+        .connect(projectOwner)
+        .deploy(
+          projectOwner.address,
+          name,
+          handle,
+          logoUri,
+          target,
+          currency,
+          duration,
+          link,
+          discountRate,
+          bondingCurveRate,
+          reserved
+        );
+      const receipt = await tx.wait();
 
       // Verify event
-      const events = receipt.events.filter(x => x.event === "Deploy");
+      const events = receipt.events.filter((x) => x.event === "Deploy");
       expect(events).to.have.lengthOf(1);
       const event = events[0];
-      expect(event).to.have.property('event').that.equals('Deploy');
-      
+      expect(event).to.have.property("event").that.equals("Deploy");
+
       // Verify  project
       // TODO: Is there a better way to get the projectId?
       const projectId = event.args.projectId;
-      const identifiers = await projects.getIdentifier(projectId);
+      const identifiers = await contracts.projects.getIdentifier(projectId);
       expect(identifiers.name).to.equal(name);
       expect(identifiers.handle).to.equal(handle);
       expect(identifiers.logoUri).to.equal(logoUri);
@@ -175,7 +83,7 @@ describe("Juicer", () => {
       // Verify budgetStore
       // TODO: Is there a better way to get the budgetId?
       const budgetId = event.args.budget.id;
-      const budget = await budgetStore.getBudget(budgetId);
+      const budget = await contracts.budgetStore.getBudget(budgetId);
       expect(budget.projectId).to.equal(projectId);
       expect(budget.target).to.equal(target);
       expect(budget.currency).to.equal(currency);
@@ -183,7 +91,6 @@ describe("Juicer", () => {
       expect(budget.reserved).to.equal(reserved);
       expect(budget.bondingCurveRate).to.equal(bondingCurveRate);
       expect(budget.discountRate).to.equal(discountRate);
-
     });
   });
 });
